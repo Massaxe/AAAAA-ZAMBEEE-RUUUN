1854799b091a36b5c65d0bea59c2c5d57e1bc6e7
Massaxe <gabbe.las.2011@gmail.com>
2015-12-09 07:56:29 +0100

    First

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform Player;
+    public float smoothDamp = 0.2f;
+    Vector3 v3 = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = new Vector3(Player.position.x, transform.position.z, Player.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position, playerPos,ref v3, smoothDamp);
+	
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float speed;
+    private Rigidbody Player;
+
+	// Use this for initialization
+	void Start () {
+        Player = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float mx = Input.GetAxisRaw("Horizontal");
+        float mz = Input.GetAxisRaw("Vertical");
+        var movement = new Vector3(mx, 0f, mz);
+
+        Player.velocity += movement * speed * Time.deltaTime;
+
+        if (movement == Vector3.zero)
+            Player.velocity = new Vector3(0, 0, 0);
+	
+	}
+}
