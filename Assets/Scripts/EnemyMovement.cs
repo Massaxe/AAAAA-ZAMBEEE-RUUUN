432719a5afe0fbfc0f8c6472e9a4fffcab6c1e4b
Massaxe <gabbe.las.2011@gmail.com>
2015-12-10 10:58:28 +0100

    10/12

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public NavMeshAgent NAgent;
+    public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+        NAgent = GetComponent<NavMeshAgent>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        NAgent.destination = goal.position;
+	
+	}
+}
---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+    public LayerMask shootable;
+    public float range = 10;
+    public RaycastHit rhit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.DrawRay(transform.position, transform.forward * 5, Color.green);
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            if(Physics.Raycast(ray, out rhit, range, shootable))
+            {
+                print("Raycast works: " + rhit);
+            }
+        }
+	
+	}
+}
